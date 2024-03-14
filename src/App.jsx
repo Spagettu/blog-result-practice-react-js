1afe2d6697a79b2a94f6de5bc2d2bb2799aaae79
Олег <oleg@192.168.1.5>
2024-03-14 16:48:05 +0300

    реализовать основной контент компонент блога с роутингом

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+const Header = () => "header";
+const Footer = () => "Footer";
+const Content = styled.div({
+  padding: "120px 0",
+});
 
-function App() {
+function Blog() {
   const [count, setCount] = useState(0);
 
-  return <div className="app">{count}</div>;
+  return (
+    <>
+      <Header />
+      <Content>
+        <Routes>
+          <Route path="/" element={<div>Главная</div>} />
+          <Route path="/login" element={<div>login</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/users" element={<div>Users</div>} />
+          <Route path="/post/:postId" element={<div>Post</div>} />
+          <Route path="/post" element={<div>New Post</div>} />
+          <Route path="*" element={<div>Error</div>} />
+        </Routes>
+      </Content>
+      <Footer />
+    </>
+  );
 }
 
-export default App;
+export default Blog;
